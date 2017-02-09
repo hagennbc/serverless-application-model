fffa884da0b38436d48de33ddfa06543b6d0702a
Hagen Dias <hagen.dias@nbcuni.com>
2017-02-08 23:21:50 -0500

    updating dev

--- examples/2016-10-31/api_swagger_cors/index.js
+++ examples/2016-10-31/api_swagger_cors/index.js
@@ -2,7 +2,7 @@ exports.handler = function(event, context, callback) {
 
     callback(null, {
         statusCode: '200',
-        body: "Hello world v2.0"
+        body: "Hello world v3.0"
     });
 
 }
